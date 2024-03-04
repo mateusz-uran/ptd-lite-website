086c50c81bce476ed5885325309faa7c3a5cb4ec
Mateusz <mateusz.uranowski@onet.pl>
2024-03-04 10:39:06 +0100

    dev branch

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>logo</li>
+        <li>
+          Funkcje{" "}
+          <ul>
+            <li>Ładunek</li>
+            <li>Faktura</li>
+            <li>Statystyki</li>
+            <li>Archiwum</li>
+            <li>Aktualizacje</li>
+          </ul>
+        </li>
+        <li>Jak to działa</li>
+        <li>Kontakt</li>
+        <li>
+          <button>Login</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
