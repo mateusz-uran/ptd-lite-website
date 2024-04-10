05fb79943fd337717042e71b74e35eda1f883b62
Mateusz <mateusz.uranowski@onet.pl>
2024-04-10 12:02:30 +0200

    feat: create animation wrapper, animate feature page

--- src/components/features/AnimationWrapper.tsx
+++ src/components/features/AnimationWrapper.tsx
@@ -1,29 +1,57 @@
-import { useInView } from "framer-motion";
-import { ReactNode, useEffect, useRef } from "react";
+import { useAnimation, useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
-type Props = {
-  children: ReactNode;
+type AnimationWrapperProps = {
+  children: JSX.Element;
+  variantType: string;
+  listIndex: number;
 };
 
-export const AnimationWrapper = ({ children }: Props) => {
+export const AnimationWrapper = ({
+  children,
+  variantType,
+  listIndex,
+}: AnimationWrapperProps) => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+
+  const variantsTypes = {
+    text: {
+      hidden: { opacity: 0, y: 100 },
+      show: (index: number) => ({
+        opacity: 1,
+        y: 0,
+        transition: { duration: 0.4, delay: 0.3 * index },
+      }),
+    },
+    images: {
+      hidden: { opacity: 0, x: 100 },
+      show: (index: number) => ({
+        opacity: 1,
+        x: 0,
+        transition: { duration: 0.4, delay: 0.3 * index },
+      }),
+    },
+  };
+
+  const selectedVariant =
+    variantsTypes[variantType as keyof typeof variantsTypes];
 
   useEffect(() => {
-    console.log(isInView);
+    if (isInView) {
+      mainControls.start("show");
+    }
   }, [isInView]);
 
   return (
-    <div ref={ref}>
+    <div ref={ref} style={{ overflow: "hidden" }}>
       <motion.div
-        variants={{
-          hidden: { opacity: 0, y: 100 },
-          show: { opacity: 1, y: 0 },
-        }}
+        variants={selectedVariant}
         initial="hidden"
-        animate="show"
-        transition={{ duration: 0.5, delay: 0.25 }}
+        animate={mainControls}
+        custom={listIndex}
       >
         {children}
       </motion.div>
--- src/components/features/FeatureSpecification.tsx
+++ src/components/features/FeatureSpecification.tsx
@@ -1,32 +1,11 @@
 import style from "./featureSpec.module.scss";
 import { featureSpecification } from "../../content/featuresContent";
 import { useParams } from "react-router-dom";
-import { motion } from "framer-motion";
 import ReactPlayer from "react-player/youtube";
+import { AnimationWrapper } from "./AnimationWrapper";
 
 const FeatureSpecification = () => {
   const { featureName } = useParams<{ featureName: string }>();
-
-  const content = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.2,
-        delayChildren: 0.3,
-      },
-    },
-  };
-
-  const text = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1 },
-  };
-
-  const images = {
-    hidden: { opacity: 0, y: -200 },
-    show: { opacity: 1, y: 0 },
-  };
 
   return (
     <>
@@ -35,66 +14,65 @@ const FeatureSpecification = () => {
           return (
             <section key={feature.id} className={style.section}>
               <div className={style.wrapper}>
-                <motion.div
-                  initial="hidden"
-                  variants={content}
-                  animate="show"
-                  className={style.content}
-                >
+                <div className={style.content}>
                   <div className={style.textWrapper}>
                     <img
                       className={style.points}
                       src="/images/features/cargo/concept-communication-with-pins-map.jpg"
                       alt="points"
                     />
-                    <motion.h3 variants={text}>{feature.header}</motion.h3>
-                    {feature.text?.map((paragraph) => (
-                      <motion.div
-                        key={paragraph}
-                        className={style.paragraphs}
-                        variants={text}
-                      >
-                        <p>{paragraph}</p>
-                      </motion.div>
-                    ))}
-                  </div>
-                  {feature.imageLinks?.map((image) => (
-                    <motion.div
-                      key={image.link}
-                      className={style.imgWrapper}
-                      variants={images}
-                    >
-                      <figure>
-                        <img src={image.link} />
-                        <figcaption>{image.caption}</figcaption>
-                      </figure>
-                    </motion.div>
-                  ))}
-                  <motion.div className={style.videoWrapper} variants={text}>
-                    <p>
-                      Film poglądowy przedstawiający krok po kroku jak funkcja
-                      działa.
-                    </p>
-                    <ReactPlayer
-                      className={style.reactPlayer}
-                      width="100%"
-                      height="100%"
-                      url="https://www.youtube.com/watch?v=SlO58lWobYM"
-                    />
-                  </motion.div>
-                  <motion.div className={style.subTextWrapper} variants={text}>
-                    {feature.subtext?.map((sub) => (
-                      <div key={sub} className={style.subText}>
-                        <p className={style.subText}>{sub}</p>
+                    <AnimationWrapper variantType="text" listIndex={1}>
+                      <h3>{feature.header}</h3>
+                    </AnimationWrapper>
+                    {feature.text?.map((paragraph, index) => (
+                      <div key={paragraph} className={style.paragraphs}>
+                        <AnimationWrapper variantType="text" listIndex={index}>
+                          <p>{paragraph}</p>
+                        </AnimationWrapper>
                       </div>
                     ))}
-                  </motion.div>
-                </motion.div>
+                  </div>
+                  {feature.imageLinks?.map((image, index) => (
+                    <div key={image.link} className={style.imgWrapper}>
+                      <AnimationWrapper variantType="images" listIndex={index}>
+                        <figure>
+                          <img src={image.link} />
+                          <figcaption>{image.caption}</figcaption>
+                        </figure>
+                      </AnimationWrapper>
+                    </div>
+                  ))}
+                  <div className={style.videoWrapper}>
+                    <AnimationWrapper variantType="images" listIndex={1}>
+                      <>
+                        <p>
+                          Film poglądowy przedstawiający krok po kroku jak
+                          funkcja działa.
+                        </p>
+                        <ReactPlayer
+                          className={style.reactPlayer}
+                          width="100%"
+                          height="100%"
+                          url="https://www.youtube.com/watch?v=SlO58lWobYM"
+                        />
+                      </>
+                    </AnimationWrapper>
+                  </div>
+                  <div className={style.subTextWrapper}>
+                    {feature.subtext?.map((sub) => (
+                      <div key={sub} className={style.subText}>
+                        <AnimationWrapper variantType="text" listIndex={1}>
+                          <p className={style.subText}>{sub}</p>
+                        </AnimationWrapper>
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
             </section>
           );
         }
-        return null; //todo: style returning object if url error appear
+        return null; //TODO: style returning object if url error appear
       })}
     </>
   );
